75c56d844845ed07ebb4f89ee01540c3eaf2c117
zengt <863468390@qq.com>
2016-07-22 14:44:07 +0800

    使用 amazon s3保存文件未完成

---
+++ src/main/java/com/kennedy/springdemo/utils/FileUploadUtil.java
@@ -0,0 +1,14 @@
+package com.kennedy.springdemo.utils;
+
+import org.springframework.web.multipart.MultipartFile;
+/**
+ * @Description: 使用amazon s3 保存文件
+ * @date: 2016年7月22日 下午2:41:41
+ * @author: zengt
+ * @version: 1.0
+ */
+public class FileUploadUtil {
+    private static void saveImage(MultipartFile file) throws Exception {
+        // TODO 缺少必要包未完成
+    }
+}
