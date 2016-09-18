696b550416d4a8b3b4a0e37f9d3fd073e56888ab
zengt <863468390@qq.com>
2016-09-18 16:43:01 +0800

    修改微信接口

---
+++ src/main/java/com/kennedy/springdemo/beans/wechat/message/Message.java
@@ -0,0 +1,10 @@
+package com.kennedy.springdemo.beans.wechat.message;
+
+/**
+ * @Description: 微信消息
+ * @date: 2016年9月6日 上午9:37:36
+ * @author: zengt
+ * @version: 1.0
+ */
+public class Message {
+}
---
+++ src/main/java/com/kennedy/springdemo/web/wechat/DynamicGetAccessToken.java
@@ -0,0 +1,63 @@
+package com.kennedy.springdemo.web.wechat;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.kennedy.springdemo.beans.wechat.Token;
+import com.kennedy.springdemo.utils.WeChatUtil;
+
+import net.sf.json.JSONException;
+
+/**
+ * @Description: Spring容器初始化完成时获取微信accessToken
+ * @date: 2016年9月2日 上午11:51:43
+ * @author: zengt
+ * @version: 1.0
+ */
+public class DynamicGetAccessToken implements ApplicationListener<ApplicationEvent> {
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+    static Boolean flag = true;
+    static int num = 0;
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+        if (flag) {
+            new Thread() {
+
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            // 获取accessToken
+                            Token token = WeChatUtil.getToken();
+                            if (StringUtils.isNotBlank(token.getAccessToken())) {
+                                redisTemplate.opsForValue().set("accessToken", token.getAccessToken());
+                                num = 0;
+                                sleep(60 * 60 * 2 * 1000);
+                            }
+                            if (num > 10) {
+                                Thread.sleep(60 * 60 * 2 * 1000);
+                            } else {
+                                num++;
+                                Thread.sleep(1000 * 3); // 获取的access_token为空
+                                                        // 休眠3秒
+                            }
+
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }.start();
+        }
+        flag = false;
+    }
+}
