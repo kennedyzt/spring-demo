82842fe1f5e5e112b989bc0a341406cfc7b872c1
kennedyzt <863468390@qq.com>
2016-10-13 21:26:57 +0800

    优化命名

--- src/main/webapp/resources/js/framework/layout/layout.js
+++ src/main/webapp/resources/js/framework/layout/layout.js
@@ -6,49 +6,50 @@ $(function() {
 });
 
 function resizeHW() {
-    var _height = $(window).height(); // 屏幕高度
-    var _width = $(window).width(); // 屏幕宽度
-    var _headerHeight = 100; // header高度
-    var _footerHeight = 100; // footer高度
-    var _menuWidth = 200; // 菜单宽度
+    var winH = $(window).height(); // 屏幕高度
+    var winW = $(window).width(); // 屏幕宽度
+    var headerH = 100; // header高度
+    var footerH = 100; // footer高度
+    var menuW = 250; // 菜单宽度
+    var navTabsH=40; // tabs高度
     $("#body").css({
-        "width" : _width,
-        "height" : _height
+        "width" : winW,
+        "height" : winH
     });
 
     $("#header").css({
         "background-color" : '#888877',
-        "width" : _width,
-        "height" : _headerHeight
+        "width" : winW,
+        "height" : headerH
     });
 
     $("#main").css({
-        "height" : _height - (_headerHeight + _footerHeight),
-        "width" : _width
+        "height" : winH - (headerH + footerH),
+        "width" : winW
     });
 
     $(".left").css({
         "overflow-y" : "scroll",
         "float" : "left",
-        "height" : _height - (_headerHeight + _footerHeight),
-        "width" : _menuWidth
+        "height" : winH - (headerH + footerH),
+        "width" : menuW
     });
 
     $(".right").css({
         "float" : "left",
-        "height" : _height - (_headerHeight + _footerHeight),
-        "width" : _width - _menuWidth
+        "height" : winH - (headerH + footerH),
+        "width" : winW - menuW
     });
 
     $(".tab-content").css({
         "float" : "left",
-        "height" : _height - (_headerHeight + _footerHeight) - 40,
-        "width" : _width - _menuWidth
+        "height" : winH - (headerH + footerH) - navTabsH,
+        "width" : winW - menuW
     });
 
     $("#footer").css({
         "background-color" : '#888877',
-        "height" : _footerHeight,
-        "width" : _width
+        "height" : footerH,
+        "width" : winW
     });
 }
